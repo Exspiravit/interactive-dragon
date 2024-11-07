1a2ea03dc36df239fbaeab6f7df7c84d839881e0
Randy JRS <randyjrs@hotmail.com>
2024-11-07 15:12:31 -0500

    feature #commnet add coments to explain main run function

--- src/main.js
+++ src/main.js
@@ -47,12 +47,27 @@ for (let i = 1; i < N; i++) {
 }
 
 const run = () => {
+  /**
+   * Llamada Recursiva:
+   * requestAnimationFrame(run); asegura que la función run se ejecute en el siguiente frame de animación, creando un bucle continuo.
+   */
   requestAnimationFrame(run);
+  // obtiene el primer elemento del array elems.
   let e = elems[0];
+  /**
+   * ax y ay son desplazamientos calculados usando funciones trigonométricas (Math.cos y Math.sin) y están escalados por rad, width, y height. Estos valores determinan cómo se moverá el primer elemento en el espacio.
+   * e.x y e.y se actualizan suavemente hacia ax + pointer.x y ay + pointer.y respectivamente, usando un factor de suavizado de 10.
+   */
   const ax = (Math.cos(3 * frm) * rad * width) / height;
   const ay = (Math.sin(4 * frm) * rad * height) / width;
   e.x += (ax + pointer.x - e.x) / 10;
   e.y += (ay + pointer.y - e.y) / 10;
+  /**
+   * Un bucle for recorre los elementos desde el segundo hasta el último (i = 1 a N-1).
+   * Para cada elemento e, se calcula el ángulo a hacia el elemento anterior ep usando Math.atan2.
+   * e.x y e.y se ajustan para seguir al elemento anterior ep, con un desplazamiento adicional basado en el ángulo a y el índice i.
+   * s es un factor de escala que disminuye con el índice i.
+   */
   for (let i = 1; i < N; i++) {
     let e = elems[i];
     let ep = elems[i - 1];
@@ -60,6 +75,9 @@ const run = () => {
     e.x += (ep.x - e.x + (Math.cos(a) * (100 - i)) / 5) / 4;
     e.y += (ep.y - e.y + (Math.sin(a) * (100 - i)) / 5) / 4;
     const s = (162 + 4 * (1 - i)) / 50;
+    /**
+     * e.use.setAttributeNS aplica una transformación SVG al elemento e, que incluye una traslación al punto medio entre e y ep, una rotación basada en el ángulo a, y una escala basada en s.
+     */
     e.use.setAttributeNS(
       null,
       "transform",
@@ -68,8 +86,15 @@ const run = () => {
       }) translate(${0},${0}) scale(${s},${s})`
     );
   }
+  /**
+   * Si rad es menor que radm, se incrementa en 1.
+   * frm se incrementa ligeramente (0.003) para animar los cálculos trigonométricos en los siguientes frames.
+   */
   if (rad < radm) rad++;
   frm += 0.003;
+  /**
+   * Si rad es mayor que 60, pointer.x y pointer.y se ajustan suavemente hacia el centro de la pantalla (width / 2, height / 2), usando un factor de suavizado de 0.05.
+   */
   if (rad > 60) {
     pointer.x += (width / 2 - pointer.x) * 0.05;
     pointer.y += (height / 2 - pointer.y) * 0.05;
